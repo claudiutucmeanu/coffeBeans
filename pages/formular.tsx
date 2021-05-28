c154de09225862da99a9929aefdcf25d7b942dcc
Claudiu Tucmeanu <claudiutucmeanu@gmail.com>
2021-05-28 13:11:20 +0300

    modified:   pages/formular.tsx

--- pages/formular.tsx
+++ pages/formular.tsx
@@ -9,7 +9,6 @@ import {
   Select,
   Button,
   Center,
-  Flex,
   FormControl,
 } from "@chakra-ui/react";
 import Styles from "../styles/formular.module.css";
@@ -20,13 +19,51 @@ export default function Home() {
   const MAPBOX_TOKEN =
     "pk.eyJ1IjoiY2xhdWRpdXR1Y21lYW51IiwiYSI6ImNrb25iZTQ4OTAxcTczMHJzdmc4MDA5MHIifQ.K9dH5SFgNqUQ_kBV2NNTQQ";
 
-  const mapRef = useRef();
-  const nameRef = useRef('').current;
-  const adressRef = useRef('').current;
-  const infoRef = useRef('').current;
-  const prajRef = useRef('').current;
-  const cafRef = useRef('').current;
-  const sectorRef = useRef('').current;
+    const mapRef = useRef();
+    const nameRef = useRef('');
+
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAdress, setEnteredAdress] = useState('');
+    const [enteredInfo, setEnteredInfo] = useState('');
+    const [enteredPraj, setEnteredPraj] = useState('');
+    const [enteredCaf, setEnteredCaf] = useState('');
+    const [enteredSector, setEnteredSector] = useState('');
+
+    // const [formIsValid, setFormIsValid] = useState(false);
+
+    const nameInputChangeHandler = (event) => {
+      setEnteredName(event.target.value);
+    };
+    const adressInputChangeHandler = (event) => {
+      setEnteredAdress(event.target.value);
+    };
+    const infoInputChangeHandler = (event) => {
+      setEnteredInfo(event.target.value);
+    };
+    const prajInputChangeHandler = (event) => {
+      setEnteredPraj(event.target.value);
+    };
+    const cafInputChangeHandler = (event) => {
+      setEnteredCaf(event.target.value);
+    };
+    const sectorInputChangeHandler = (event) => {
+      setEnteredSector(event.target.value);
+    };
+
+    // const enteredNameIsValid = enteredName.trim() !== '';
+    // const enteredAdressIsValid = enteredAdress.trim() !== '';
+    // const enteredInfoIsValid = enteredInfo.trim() !== '';
+    // const enteredPrajIsValid = enteredPraj.trim() !== '';
+    // const enteredCafIsValid = enteredCaf.trim() !== '';
+    // const enteredSectorIsValid = enteredSector.trim() !== '';
+    
+
+    let formIsValid=false;
+    
+
+    // if (enteredNameIsValid && enteredAdressIsValid && enteredInfoIsValid && enteredPrajIsValid && enteredCafIsValid && enteredSectorIsValid) {
+    //   formIsValid=true;      
+    // }
 
   const [viewport, setViewport] = useState({
     latitude: 44.4403,
@@ -51,20 +88,33 @@ export default function Home() {
     [handleViewportChange]
   );
 
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const enteredAdressIsValid = enteredAdress.trim() !== '';
+  const enteredInfoIsValid = enteredInfo.trim() !== '';
+  const enteredPrajIsValid = enteredPraj.trim() !== '';
+  const enteredCafIsValid = enteredCaf.trim() !== '';
+  const enteredSectorIsValid = enteredSector.trim() !== '';
+  const latitudeIsValid = viewport.latitude != 44.4403;
+  const longitudeIsValid = viewport.longitude != 26.1025;
+
+  if (enteredNameIsValid && enteredAdressIsValid && enteredInfoIsValid && enteredPrajIsValid && enteredCafIsValid && enteredSectorIsValid && latitudeIsValid && longitudeIsValid) {
+    formIsValid=true;      
+  }
+
+
   const submitHandler = async (event) => {
     event.preventDefault();
     const cafenea = {
-      name: nameRef,
-      adress: adressRef,
-      info: infoRef,
-      praj: prajRef,
-      caf: cafRef,
-      sector: sectorRef,
+      name: enteredName,
+      adress: enteredAdress,
+      info: enteredInfo,
+      praj: enteredPraj,
+      caf: enteredCaf,
+      sector: enteredSector,
       lat: viewport.latitude,
       lon: viewport.longitude,
     };
-
-    // console.log(cafenea);
 
     const response = await fetch(
       "https://coffeebeans-bebab-default-rtdb.firebaseio.com/shops.json",
@@ -76,8 +126,13 @@ export default function Home() {
         },
       }
     );
-    const data = await response.json();
-    console.log(data);
+    
+     setEnteredName('');
+     setEnteredAdress('');
+     setEnteredInfo('');
+     setEnteredPraj('');
+     setEnteredCaf('');
+     setEnteredSector('');
   };
 
   return (
@@ -87,21 +142,21 @@ export default function Home() {
         <form className={Styles.form} onSubmit={submitHandler}>
           <FormControl isRequired>
             <FormLabel marginTop="3px">Nume</FormLabel>
-            <Input type="text" id="name" ref={nameRef.value} />
+            <Input type="text" id="name" onChange={nameInputChangeHandler} value={enteredName} />
           </FormControl>
           <FormControl isRequired>
           <FormLabel marginTop="4px">Adresă</FormLabel>
-          <Input type="text" id="adress" ref={adressRef.value} />
+          <Input type="text" id="adress" onChange={adressInputChangeHandler} value={enteredAdress} />
           </FormControl>
           <FormControl isRequired>
           <FormLabel marginTop="4px">Info</FormLabel>
-          <Textarea type="text" id="info" ref={infoRef.value} />
+          <Textarea type="text" id="info" onChange={infoInputChangeHandler} value={enteredInfo} />
           </FormControl>
           <FormControl isRequired>
           <FormLabel marginTop="4px">
             Prajitorie proprie
           </FormLabel>
-          <Select placeholder="Alege optiune" id="praj" ref={prajRef.value}>
+          <Select placeholder="Alege optiune" id="praj" onChange={prajInputChangeHandler} value={enteredPraj} >
             <option>Da</option>
             <option>Nu</option>
           </Select>
@@ -110,14 +165,14 @@ export default function Home() {
           <FormLabel marginTop="4px">
             Cafenea
           </FormLabel>
-          <Select placeholder="Alege optiune" id="caf" ref={cafRef.value}>
+          <Select placeholder="Alege optiune" id="caf" onChange={cafInputChangeHandler} value={enteredCaf} >
             <option>Da</option>
             <option>Nu</option>
           </Select>
           </FormControl>
           <FormControl isRequired>
           <FormLabel marginTop="4px">Sector</FormLabel>
-          <Select placeholder="Alege sectorul" id="sector" ref={sectorRef.value}>
+          <Select placeholder="Alege sectorul" id="sector" onChange={sectorInputChangeHandler} value={enteredSector} >
             <option>Sector 1</option>
             <option>Sector 2</option>
             <option>Sector 3</option>
@@ -155,6 +210,7 @@ export default function Home() {
               colorScheme="blackAlpha"
               variant="outline"
               type="submit"
+              disabled={!formIsValid}
             >
               Submit
             </Button>
